f027991bfa861a4b4cb8299d38cec7a1d6a01c9a
Arnav Ranjan <arnav.ranjan@kimballmidwest.com>
2025-08-09 01:49:43 +0530

    Handle division by zero with error checking
    Adds error handling to the integer division function to prevent division by zero. Updates the main logic to handle errors gracefully and print appropriate messages. Improves code readability with updated import formatting.

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "errors"
+)
 
 func main(){
     var printvalue string = "Hello, World!"
     printMe(printvalue)
 
     var numerator int = 10
-    var denominator int = 2
-    var result, remainder int = intdivision(numerator, denominator)
+    var denominator int = 3
+    var result, remainder, err = intdivision(numerator, denominator)
+    if err!=nil {
+        fmt.Printf(err.Error())
+    }else if remainder == 0{
+        fmt.Printf("Amaze")
+    }
     fmt.Printf("the result of the integer division is %v with remainder %v\n", result, remainder)
 }
 
@@ -16,8 +24,13 @@ func printMe(printvalue string){
     fmt.Println(printvalue)
 }
 
-func intdivision(numerator int, denominator int) (int, int){
+func intdivision(numerator int, denominator int) (int, int, error){
+    var err error
+    if denominator == 0 {
+        err = errors.New("division by zero so we cannot divide mate!")
+        return 0, 0, err
+    }
     var result int = numerator/denominator
     var remainder int = numerator%denominator
-    return result, remainder
+    return result, remainder , err
 }
