257f8763615398fb32aed8e969204b95fc35872b
Arnav Ranjan <arnav.ranjan@kimballmidwest.com>
2025-08-09 01:36:26 +0530

    Adds integer division functionality with result and remainder
    Introduces a new function to perform integer division, returning both the result and the remainder. Demonstrates its usage in the `main` function with formatted output. Enhances program functionality by expanding its computational capabilities.

--- main.go
+++ main.go
@@ -5,8 +5,19 @@ import "fmt"
 func main(){
     var printvalue string = "Hello, World!"
     printMe(printvalue)
+
+    var numerator int = 10
+    var denominator int = 2
+    var result, remainder int = intdivision(numerator, denominator)
+    fmt.Printf("the result of the integer division is %v with remainder %v\n", result, remainder)
 }
 
 func printMe(printvalue string){
     fmt.Println(printvalue)
+}
+
+func intdivision(numerator int, denominator int) (int, int){
+    var result int = numerator/denominator
+    var remainder int = numerator%denominator
+    return result, remainder
 }
