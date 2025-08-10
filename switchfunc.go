44ddfc74c44e2bffeb9d8857cd35dec61a2c8f7a
Arnav1511 <meetarnavranjan@gmail.com>
2025-08-10 17:22:52 +0530

    Removes unused main.go file
    Deletes the main.go file, which contained example code for integer division and printing. Likely removed as part of a cleanup or refactoring effort to eliminate unused or obsolete files.

---
+++ switchfunc.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func main() {
+	var printvalue string = "Hello, World!"
+	printMe(printvalue)
+
+	var numerator int = 10
+	var denominator int = 2
+	var result, remainder, err = intdivision(numerator, denominator)
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	switch remainder {
+	case 0:
+		fmt.Printf("the division was exact: %d / %d = %d\n", numerator, denominator, result)
+	case 1, 2:
+		fmt.Printf("the division was close")
+	default:
+		fmt.Printf("the division was not close")
+	}
+}
+
+func printMe(printvalue string) {
+	fmt.Println(printvalue)
+}
+
+func intdivision(numerator int, denominator int) (int, int, error) {
+	var err error
+	if denominator == 0 {
+		err = errors.New("division by zero so we cannot divide mate")
+		return 0, 0, err
+	}
+	var result int = numerator / denominator
+	var remainder int = numerator % denominator
+	return result, remainder, err
+}
