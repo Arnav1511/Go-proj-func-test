e3e702957939743916a00c6650771b4f3e6d5cf9
Arnav1511 <meetarnavranjan@gmail.com>
2025-08-13 03:01:15 +0530

    more arrays

--- array/arrays.go
+++ array/arrays.go
@@ -12,7 +12,20 @@ func main() {
 
 	var intSlice []int32 = intArr[1:3]
 	fmt.Println(intSlice)
+	// we can print out the length and capacity of the slice
+
+	fmt.Println("Length:", len(intSlice))
+	fmt.Println("Capacity:", cap(intSlice))
 
 	intSlice = append(intSlice, 12, 13, 14)
+	fmt.Println(intSlice)
+
+	// we can print out the length and capacity of the slice
+	fmt.Println("Length:", len(intSlice))
+	fmt.Println("Capacity:", cap(intSlice))
+
+	//we can also append a slice to another slice so let me show you that as well below :)
+	intSlice2 := []int32{15, 16, 17}
+	intSlice = append(intSlice, intSlice2...)
 	fmt.Println(intSlice)
 }
