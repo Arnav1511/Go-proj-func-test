87c5e079419ddfe093134702d087e19bd61db0ce
Arnav. <46954436+Arnav1511@users.noreply.github.com>
2025-08-15 23:50:22 +0530

    Update arrays.go

--- array/arrays.go
+++ array/arrays.go
@@ -36,6 +36,7 @@ func main() {
 	//but you don't want to use the array syntax
 	//so we can use the make function to create a slice from an array
 	//and then copy the elements from the array to the slice
+	//testing slices in go lang
 	intSlice3 := make([]int32, 2)
 	copy(intSlice3, intArr[1:3])
 	fmt.Println(intSlice3)
