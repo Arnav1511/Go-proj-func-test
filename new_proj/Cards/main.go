733d447d9e0aa1c7849cd490659440dffe88ffa7
Arnav1511 <meetarnavranjan@gmail.com>
2025-08-19 16:47:33 +0530

    Add global cards variable and update main function to print it

--- new_proj/Cards/main.go
+++ new_proj/Cards/main.go
@@ -4,8 +4,11 @@ import (
 	"fmt"
 )
 
+var cards = "Ace of Spades"
+
 func main() {
 	card := "Ace of Spades"
 	card = "King of Hearts" // Example of modifying the card variable
 	fmt.Println(card)
+	fmt.Println(cards) // Print the global cards variable
 }
