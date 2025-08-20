969b5f521382d55dbdad92409193e88b7935ebd5
Arnav1511 <meetarnavranjan@gmail.com>
2025-08-20 16:49:32 +0530

    Refactor main function to initialize cards using newDeck and remove unused newCard function

--- new_proj/Cards/main.go
+++ new_proj/Cards/main.go
@@ -2,13 +2,8 @@ package main
 
 func main() {
 
-	cards := deck{"Ace of Diamond", newCard()}
-	cards = append(cards, "Six of Spades")
+	cards := newDeck()
 
 	// Print the cards in hand
 	cards.print()
-}
-
-func newCard() string {
-	return "Ace of Spades"
 }
