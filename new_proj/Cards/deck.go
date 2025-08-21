35dac42084d2e26c0b120dd2b738db53b2b697e0
Arnav1511 <meetarnavranjan@gmail.com>
2025-08-21 17:29:11 +0530

    Refactor main to load cards from file and update saveToFile implementation to use os package

--- new_proj/Cards/deck.go
+++ new_proj/Cards/deck.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"os"
 	"strings"
 )
 
@@ -44,5 +44,18 @@ func (d deck) toString() string {
 }
 
 func (d deck) saveToFile(filename string) error {
-	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+	return os.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := os.ReadFile(filename)
+	if err != nil {
+		//option 1 - log the error and return a call to the newDeck()
+		//option 2 - log the error and exit the program
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(bs), ",") // ace of spades, two of spades, three of spades
+	return deck(s)
 }
