36dc9b0cd72b8a375c6364c0d38e6c229b96a2b7
Arnav1511 <meetarnavranjan@gmail.com>
2025-08-25 02:36:41 +0530

    Refactor main function to comment out unused code and utilize print method for person struct

--- structs/main.go
+++ structs/main.go
@@ -14,16 +14,20 @@ type person struct {
 }
 
 func main() {
-	var alex person
-	alex.firstName = "Alex"
-	alex.lastName = "Anderson"
-	alex.contactInfo = contactInfo{email: "alex@hotvscode.com", zipCode: 94400}
-	fmt.Printf("%+v", alex)
+	// var alex person
+	// alex.firstName = "Alex"
+	// alex.lastName = "Anderson"
+	// alex.contactInfo = contactInfo{email: "alex@hotvscode.com", zipCode: 94400}
+	// fmt.Printf("%+v", alex)
 
 	jim := person{
 		firstName:   "Jim",
 		lastName:    "Jimothy",
 		contactInfo: contactInfo{email: "Jimothy@hotvscode.com", zipCode: 94400},
 	}
-	fmt.Printf("%+v", jim)
+	jim.print()
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
