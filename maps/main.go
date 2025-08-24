5565c966bab029a2c5defbe717dffe71a8301457
Arnav1511 <meetarnavranjan@gmail.com>
2025-08-25 04:38:42 +0530

    Format colour map for improved readability and add missing newline at end of file

--- maps/main.go
+++ maps/main.go
@@ -3,11 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	colours :=map[string]string{
-		"red":"s#ff0000",
-		"green":"#00ff00",
-		"blue":"#0000ff",
+	colours := map[string]string{
+		"red":   "s#ff0000",
+		"green": "#00ff00",
+		"blue":  "#0000ff",
 	}
-		fmt.Println(colours)
 
+	fmt.Println(colours)
 }
