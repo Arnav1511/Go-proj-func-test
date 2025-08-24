68cccebc081926a80691b3c0aa92574a8cff872b
Arnav1511 <meetarnavranjan@gmail.com>
2025-08-25 02:32:26 +0530

    Refactor person struct to include contactInfo and update main function to initialize contact details

--- structs/main.go
+++ structs/main.go
@@ -2,15 +2,28 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
 	var alex person
 	alex.firstName = "Alex"
 	alex.lastName = "Anderson"
-	fmt.Println(alex)
-	fmt.Printf("%+v\n", alex)
+	alex.contact = contactInfo{email: "alex@hotvscode.com", zipCode: 94400}
+	fmt.Printf("%+v", alex)
+
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Jimothy",
+		contact:   contactInfo{email: "Jimothy@hotvscode.com", zipCode: 94400},
+	}
+	fmt.Printf("%+v", jim)
 }
