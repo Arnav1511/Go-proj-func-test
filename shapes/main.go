2a5a21721b225bac6b1a9a9f47367e1efa48ea09
Arnav1511 <meetarnavranjan@gmail.com>
2025-08-27 02:30:52 +0530

    area of shapes using interfaces

---
+++ shapes/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+type shape interface {
+	getArea() float64
+}
+
+type square struct {
+	sideLength float64
+}
+type triangle struct {
+	height float64
+	base   float64
+}
+
+func main() {
+	t := triangle{height: 10, base: 5}
+	s := square{sideLength: 4}
+
+	printArea(t)
+	printArea(s)
+
+}
+
+func (s square) getArea() float64 {
+	return s.sideLength * s.sideLength
+}
+func (t triangle) getArea() float64 {
+	return 0.5 * t.height * t.base
+}
+func printArea(s shape) {
+	fmt.Println(s.getArea())
+}
