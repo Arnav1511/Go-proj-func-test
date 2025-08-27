4278ec9accfbf665f23f0cce1da554060508fe4b
Arnav1511 <meetarnavranjan@gmail.com>
2025-08-28 00:04:35 +0530

    trying to think of a way to fix using for loop

--- channels/main.go
+++ channels/main.go
@@ -26,4 +26,4 @@ func checkLink(links string) {
 	println(links, "is up!")
 }
 
-// The above is not the best approach for this so some sort of a parallel approach would be better.
+// The above is not the best approach for this so some sort of a parallel approach would be better.//
